195a274be195e7b3dfa2caf1c24655e0bb4c3fb8
Peter <99065921@mydavinci.nl>
2021-02-16 09:33:19 +0100

    added page name

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
     define("OUTPUT", "Hello World!");
+    
     $learning = "Learning PHP";
     $learning = OUTPUT;
 
@@ -15,7 +16,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AO-B3-W1-L2</title>
 </head>
 <body>
     <h1><?php echo "Hello World!"; ?></h1>
